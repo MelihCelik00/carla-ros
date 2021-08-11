6b6c2f1669bd6bda7fc661c0eb37c04ffec2e1a1
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2021-08-11 17:54:41 +0300

    Added new publishers to look for neighbor lanes

--- lane_publish.py
+++ lane_publish.py
@@ -29,7 +29,11 @@ def vehicleInfoCallback(data):
 if __name__ == '__main__': 
     rospy.init_node("lane_type_publish")
     rospy.Subscriber("/carla/ego_vehicle/vehicle_info", CarlaEgoVehicleInfo, vehicleInfoCallback)
-    pub = rospy.Publisher("/vehicle/current_lane_type", String, queue_size=10)
+    pubLaneType = rospy.Publisher("carla/vehicle/current_lane_type", String, queue_size=10)
+    pubLaneID = rospy.Publisher("carla/vehicle/current_road_id", String, queue_size=10)
+    pubLeftLaneType = rospy.Publisher("carla/vehicle/left_lane_type", String, queue_size=10)
+    pubRightLaneType = rospy.Publisher("carla/vehicle/right_lane_type", String, queue_size=10)
+    laneChangePub = rospy.Publisher("carla/vehicle/changeable_lane", String, queue_size=10)
     rate = rospy.Rate(10)
     
     client = carla.Client('localhost', 2000)
@@ -43,13 +47,33 @@ if __name__ == '__main__':
 
     while not rospy.is_shutdown():
         try:
-            _current_lane_info = vehicle.get_world().get_map().get_waypoint(vehicle.get_location())
+            _current_lane_info = vehicle.get_world().get_map().get_waypoint(vehicle.get_location(), project_to_road = True, lane_type=carla.LaneType.Any)
+            print("lane id: ",  _current_lane_info.lane_id)
+            print("lane type: ")
             print(_current_lane_info.lane_type)
             #print(_current_lane_info)
-            #print("Right Lane Type: ",_current_lane_info.get_right_lane())
+            try:
+                left_lane = _current_lane_info.get_left_lane().lane_type
+                right_lane = _current_lane_info.get_right_lane().lane_type
+                print("Left Lane Type: ")
+                print(str(left_lane))
+                print("Right Lane Type: ")
+                print(right_lane)
+            except AttributeError:
+                print("None type can't have change_lane_type!!")
+
+            lane_change = _current_lane_info.lane_change
+            print("lane change: ")
+            print(lane_change)
+
             time.sleep(1.0)
 
-            pub.publish(str(_current_lane_info.lane_type))
+            pubLaneType.publish(str(_current_lane_info.lane_type))
+            pubLaneID.publish(str(_current_lane_info.road_id))
+            pubLeftLaneType.publish(str(left_lane))
+            pubRightLaneType.publish(str(right_lane))
+            laneChangePub.publish(str(lane_change))
             rate.sleep()
         except rospy.ROSInterruptException:
             pass
+        
